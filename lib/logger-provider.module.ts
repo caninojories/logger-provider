b48fb2e2a763692c77bc9fc58d9db3920ac94aae
caninojories <caninojories@hotmail.com>
2022-08-28 15:23:09 +0800

    Add @elastic/elasticsearch module

--- lib/logger-provider.module.ts
+++ lib/logger-provider.module.ts
@@ -2,16 +2,33 @@ import { LoggerService } from '@nestjs/common';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 import ecsFormat = require('@elastic/ecs-winston-format');
+import {
+  ElasticsearchTransport,
+  ElasticsearchTransportOptions,
+} from 'winston-elasticsearch';
+import { Client, ClientOptions } from '@elastic/elasticsearch';
+
+type LoggerProviderOptions = {
+  level?: string;
+  esClient?: ClientOptions;
+};
 
 export class LoggerProviderModule {
-  static register({ level = 'info' }: { level?: string }): LoggerService {
+  static register({
+    level = 'info',
+    esClient = {},
+  }: LoggerProviderOptions): LoggerService {
+    const options: ElasticsearchTransportOptions = {
+      level,
+      format: winston.format.combine(ecsFormat({ convertReqRes: true })),
+    };
+
+    if (Object.keys(esClient).length) {
+      options.client = new Client(esClient);
+    }
+
     return WinstonModule.createLogger({
-      transports: [
-        new winston.transports.Console({
-          format: winston.format.combine(ecsFormat({ convertReqRes: true })),
-          level,
-        }),
-      ],
+      transports: [new ElasticsearchTransport(options)],
     });
   }
 }
